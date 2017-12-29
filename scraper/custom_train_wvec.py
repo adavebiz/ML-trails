4cedfc94cfc7f67fc4983b6e1961c9142b8820e4
Dave <adave@informatica.com>
2017-12-29 01:37:11 -0800

    Adding model creation on scraped data
    Adding the merged file created by merging all the scraped data

--- scraper/custom_train_wvec.py
+++ scraper/custom_train_wvec.py
@@ -8,18 +8,15 @@ import multiprocessing
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 
-def train(input_file,output_file,output_file2):
+def train(input_file,output_file):
 
     model = Word2Vec(LineSentence(input_file), size=200, window=5, min_count=5,
                      workers=multiprocessing.cpu_count())
-
     model.save(output_file)
-    model.wv.save_word2vec_format(output_file2, binary=False)
-
 
 def main():
-	input_file = '../../scrapedata/' +'legal_Administrative_centre.txt'
+	input_file = '../../scrapedata/'+'merged_file.txt'
 	print (input_file)
-	train(input_file,'./of1','./of2')
+	train(input_file,'./legal.health.model')
 
 main()
