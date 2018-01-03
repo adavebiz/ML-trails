cf155e053b09344de5e121d46f4c0d09b79e91fb
Dave <adave@informatica.com>
2018-01-03 03:53:00 -0800

    adding visualization using 2D projection of w2vec vectors
    TODO: Need to fix the visualization per class label

--- User-Specific-Models/visualize_word_vectors.py
+++ User-Specific-Models/visualize_word_vectors.py
@@ -1,6 +1,16 @@
 import driver_class as dr
 import sys  
 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.decomposition import PCA as sklearnPCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.datasets.samples_generator import make_blobs
+from pandas.tools.plotting import parallel_coordinates
+from pandas import DataFrame
+
+
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
@@ -25,10 +35,17 @@ def populate_category_vectors():
 		'legal': 'legal/sample_merged_file_legal.txt',
 		'health':'health/sample_merged_file_health.txt',
 	}
+	label_dict={
+		'legal':0,
+		'health':1
+	}
 	root_path = '../../sampledata'
 	model_path = '../../pretrained_models/GoogleNews-vectors-negative300-SLIM.bin'
 	vectors_dict={}
 	vocab_n_words = 10
+
+	data_matrix = []
+	labels = []
 	for category in cat_dict:
 
 		text = get_text_from_file('/'.join([root_path,cat_dict[category]]))
@@ -37,6 +54,48 @@ def populate_category_vectors():
 		print 'cat={} , size = {},{}'.format(category,len(vectors),len(vectors[0]))
 
 
+		if len(data_matrix) == 0:
+			data_matrix = vectors
+			labels = [label_dict[category]]*len(vectors)
+		else:
+			for v in vectors:
+				data_matrix.append(v)
+				labels.append(label_dict[category])
+	
+
+	return (data_matrix, labels)
+
+			
+def make_2d_proj(data_mat,n_dim):
+	X = DataFrame(data_mat)
+	#X = X.transpose()
+	X_norm = (X - X.min())/(X.max() - X.min())
+	pca = sklearnPCA(n_components=n_dim) #2-dimensional PCA
+	transformed = pd.DataFrame(pca.fit_transform(X_norm))
+	return transformed
+
+def plot_data(X,y):
+	print 'testing complete'
+	print type(X.shape)
+	print type(y.shape)
+
+	# c = y
+	print (X.iloc[:,0].shape)
+	print (X.iloc[:,1].shape)
+	# figure out the colours
+	# Try 2 separate plots
+	# try 1 plot over the other
+	plt.scatter(X.iloc[:,0], X.iloc[:,1],c = y.iloc[:,0])
+	plt.show()
+
+
 # main
-populate_category_vectors()
+(data_matrix, labels) = populate_category_vectors()
+
+X_rot = make_2d_proj(data_matrix,2)
+y = DataFrame(labels)
+plot_data(X_rot,y)
+
+
+
 
